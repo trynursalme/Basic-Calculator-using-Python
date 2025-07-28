c18d313f37885d5e91a2676dee9a315ee813e2e3
Nurselam  Hussen - ZOA Ethiopia <n.hussen@zoa.ngo>
2025-07-28 16:46:00 +0300

    finish the calculator function

--- Calc.py
+++ Calc.py
@@ -1,2 +1,34 @@
 # Basic Calculator Program
-print("Hello World")
+def calculator():
+    print("Simple Calculator using Python")
+    
+    # Get user input
+    try:
+        num1 = float(input("\nEnter the first Number:"))
+        num2 = float(input("Enter the second Number:"))
+        operation = input("Enter the operation (+, -, *, /)").strip()
+        
+        # perform calculation operation
+        if operation == "+":
+            result = num1 + num2
+            print(f"{num1}+ {num2} = {result}")
+        elif operation == "-":
+            result = num1 - num2
+            print(f"{num1} - {num2} = {result}")
+        elif operation == "*":
+            result = num1 * num2
+            print(f"{num1} * {num2} = {result}")
+        elif operation == "/":
+            if num2 == 0:
+                print("Error: Division by zero is not allowed.")
+            else:
+                result = num1 / num2
+                print(f"{num1} / {num2} = {result}")
+        else:
+            print("Invalid operation Please choose one of: +, -, *, /")
+        
+    except ValueError:
+        print("Invalid input Please Enter valid number")
+
+# Run the calculator
+calculator()
